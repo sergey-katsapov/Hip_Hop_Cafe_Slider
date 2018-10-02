8a1a8729433c39eda0781f7ba3fb549c6f457268
DESKTOP-92TDLT7\sergey_katsapov <katsapov.1996@mail,ru>
2018-10-02 23:31:47 +0300

    Hip_Hop_Start

---
+++ HipHopCafeSlider/app/src/main/java/info/hiphopcafe/launcherslider/MainActivity.java
@@ -0,0 +1,29 @@
+package info.hiphopcafe.launcherslider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+                prefManager.setFirstTimeLaunch(true);
+                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Hip_Hop_Cafe_Slider
+==============================================================================
+#### What is implemented:
+- We make the first part of our big project.
+Made a slide for a spectacular transition to the application.
+------------------------------------------------------------------------------
+### Android documentation:
+- Language: Java
+- Minimal SdkVersion: 15
+- Target SdkVersion: 23
+- Compile SdkVersion: 23
+- com.android.support:animated-vector-drawable-23.2.1
+  
+ _All  questions_ - [MY  TELEGRAM](https://t.me/sergey_katsapov)
+------------------------------------------------------------------------------
+### Application work on my  smartphone:
+![2](https://user-images.githubusercontent.com/29036196/43864558-ad15fc20-9b68-11e8-8b75-5db460292c1b.gif)
